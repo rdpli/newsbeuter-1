c6cc4c9f5656872c9a75cd73a5e779b919ca56d4
Jordan Arsenault <jarsenault@semios.com>
2015-08-21 19:47:16 -0700

    adding opml for podcast android test with git

---
+++ template.php
@@ -0,0 +1,13 @@
+<? echo "<?xml version="1.0" encoding="utf-8" ?>"; ?>
+
+<opml version="1.1">
+<head>   
+  <title>jarse-pods</title>   
+</head>
+<body>
+  <outline text="Feeds">
+    <outline title="DevHell" text="DevHell" type="rss" xmlUrl="http://feeds.devhell.info/devhell-podcast" />
+    <outline title="BRandT" text="Beats Rye and Types" type="rss" xmlUrl="http://feeds.feedburner.com/BeatsRyeTypes" />
+  </outline>
+</body>
+</opml>
